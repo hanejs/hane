53fd6e5d6da531416dc786227f77b43dff05fd9b
tengattack <tengattack@foxmail.com>
2017-02-08 16:37:43 +0800

    Add hanejs theme runtime

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ require('./lib')
 
 hane.readConfig()
 require('../hane-render-markdown')
+require('../hane-theme-simple')
 
-hane.runtime.render.feed('# Render test')
-  .then(result => {
-    console.log(result)
+async function main() {
+  const { render, theme } = hane.runtime
+  const result = await render.feed('# Render test')
+  const html = await theme.render({
+    categories: [],
+    items: [
+      {
+        title: 'Hello World',
+        content: result,
+        pubDate: new Date().toString(),
+      }
+    ],
+    tags: [],
   })
+  console.log(html)
+}
+
+main()
--- lib/theme.es
+++ lib/theme.es
@@ -1,4 +1,19 @@
 
 export default class HaneTheme {
-  
+  static register(renderClass) {
+    hane.runtime.theme = new renderClass()
+  }
+  constructor(options) {
+    this.config = {}
+    this.options = options
+    this.data = {}
+    this.initialContentType = 'index'
+  }
+  setContenType(v) {
+    this.initialContentType = v
+  }
+  render(data) {
+    this.data = data || {}
+    return ''
+  }
 }
